e2447e5224ccc4940507215c5c02e45b9a41324e
Amal Babu <amalbabu1200@gmail.com>
2025-04-15 13:22:59 +0530

    portfolio

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Amal Babu · ABK Datalab";
+export const SITE_DESCRIPTION =
+    "Welcome to my personal blog. I Write about Software Engineering, Development, ComfyUI and AI.";
